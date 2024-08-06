da7bddae284e60d7f372bbbae0d89c62f43ca73c
Charlton Groves <charlieg@canva.com>
2024-08-06 13:18:20 +1000

    Arig chase

--- src/cat2.ts
+++ src/cat2.ts
@@ -2,31 +2,69 @@ import cat1 from './assets/cat1.gif';
 import cat2 from './assets/cat2.gif';
 import cat3 from './assets/cat3.webp';
 import cat4 from './assets/cat4.webp';
+import cat5 from './assets/cat5.png';
+import cat6 from './assets/cat6.png';
 import meow1 from './assets/meow1.mp3';
 import meow2 from './assets/meow2.mp3';
 import meow3 from './assets/meow3.mp3';
 import yippee from './assets/yippee.mp3';
 import meow4 from './assets/meow4.mp3';
 import bonk from './assets/bonk.mp3';
+import kiss from './assets/kiss.mp3';
+import purr from './assets/purr.mp3';
+import grab0 from './assets/grab0.png';
+import grab1 from './assets/grab1.png';
+import grab2 from './assets/grab2.png';
+import grab3 from './assets/grab3.png';
 
-const catSrcs = [cat1, cat2, cat3, cat4];
+const catSrcs = [cat1, cat2, cat3, cat4, cat5, cat6];
+const catN = catSrcs.length;
 const meowSrcs = [meow1, meow2, meow3, meow4];
 const catSounds = ['miieeeow', 'miaow', 'maoew', 'meow']
-type Cat = {
+type Entity = {
+    type: "arig" | "cat";
     x: number;
     y: number;
-    width: number,
-    height: number,
+    width: number;
+    height: number;
     xSpeed: number;
     ySpeed: number;
     node: HTMLImageElement;
-    meowChance: number;
     text?: {
         time: number;
         text: string;
         node: null | HTMLDivElement;
     }
 }
+
+type Cat = Entity & {
+    type: "cat";
+    meowChance: number;
+}
+
+type Arig = {
+    type: "arig";
+    x: number;
+    y: number;
+    keyFrames?: {func?: () => void, time: number, src: string}[]
+    width: number,
+    height: number,
+    xSpeed: number;
+    ySpeed: number;
+    heldCats: Map<Cat, number>;
+    node: HTMLImageElement;
+    holdingCat: Cat | null;
+    dontNeedCat?: number;
+    text?: {
+        time: number;
+        text: string;
+        node: null | HTMLDivElement;
+    }
+}
+
+function getRand<T>(arr: T[]): T{
+    return arr[Math.floor(Math.random() * arr.length)];
+} 
 
 async function loadImage(url: string, elem: HTMLImageElement) {
     return new Promise((resolve, reject) => {
@@ -35,21 +73,40 @@ async function loadImage(url: string, elem: HTMLImageElement) {
         elem.src = url;
     });
 }
-function playSound(src: string, text: string, cat: Cat, vol: number = 1) {
-    const audio = new Audio(src);
+const audioCache = new Map<string, HTMLAudioElement[]>();
+
+function playSound(src: string, text: string, cat: Entity, vol: number = 1) {
+    if (!audioCache.has(src)) {
+        audioCache.set(src, []);
+    }
+    const pool = audioCache.get(src)!;
+    let audio = pool.find((a) => a.ended);
+    if (!audio) {
+        audio = new Audio(src);
+        pool.push(audio);
+    }
     audio.volume = vol;
+    // start audio from start
+    audio.currentTime = 0;
     audio.play();
     if (text) {
         addText(text, cat);
     }
-    audio.onended = () => {
-        audio.src = '';
-        audio.remove();
+}
+
+const wantsCat = (arig: Arig, cat: Cat) => {
+    const lastHeld = arig.heldCats.get(cat);
+    const dontNeed = arig.dontNeedCat && arig.dontNeedCat > Date.now();
+    if (lastHeld && lastHeld > Date.now() - 12000) {
+        return false;
+    } else {
+        return !dontNeed;
     }
 }
+
 async function createCat(x: number, y: number): Promise<Cat> {
     const cat = new Image();
-    const src = catSrcs[Math.floor(Math.random() * catSrcs.length)];
+    const src = getRand(catSrcs)
     await loadImage(src, cat)
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
@@ -57,13 +114,14 @@ async function createCat(x: number, y: number): Promise<Cat> {
     cat.style.top = `0px`;
     cat.style.left = `0px`;
     cat.style.transform = `translate(${x * viewportWidth}px, ${y * viewportHeight}px)`;
-    cat.style.width = '54px';
-    cat.style.height = '96px';
-    const newCat = {
+    cat.style.width = '80px';
+    cat.style.height = '140px';
+    const newCat: Cat = {
+        type: "cat",
         x,
         y,
-        width: 54 / viewportWidth,
-        height: 96 / viewportHeight,
+        width: 80 / viewportWidth,
+        height: 140 / viewportHeight,
         xSpeed: Math.random() * 0.02 - 0.01,
         ySpeed: Math.random() * 0.02 - 0.02,
         node: cat,
@@ -74,11 +132,40 @@ async function createCat(x: number, y: number): Promise<Cat> {
     return newCat;
 }
 
+async function createArig(x: number, y: number): Promise<Arig> {
+    const cat = new Image();
+    const src = grab0;
+    await loadImage(src, cat)
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    cat.style.position = 'absolute';
+    cat.style.top = `0px`;
+    cat.style.left = `0px`;
+    cat.style.transform = `translate(${x * viewportWidth}px, ${y * viewportHeight}px)`;
+    cat.style.width = '120px';
+    cat.style.height = '200px';
+    const newCat: Arig = {
+        type: "arig",
+        x,
+        y,
+        heldCats: new Map(),
+        width: 120 / viewportWidth,
+        height: 200 / viewportHeight,
+        xSpeed: Math.random() * 0.02 - 0.01,
+        ySpeed: Math.random() * 0.02 - 0.02,
+        node: cat,
+        holdingCat: null,
+    }
+    // play yippee sound
+    playSound(yippee, 'Yippee!', newCat);
+    return newCat;
+}
+
 let time = Date.now();
 let xAccel = 0;
 let yAccel = 1;
 
-const addText = (text: string, cat: Cat) => {
+const addText = (text: string, cat: Entity) => {
     if (cat.text?.node) {
         document.body.removeChild(cat.text.node);
     }
@@ -97,16 +184,16 @@ const addText = (text: string, cat: Cat) => {
         node: textNode,
     }
 }
-const updateCats = (cats: Cat[]) => {
+const updateEntities = (parent: HTMLElement, cats: (Arig | Cat)[]) => {
 
     // percentage of viewport height per frame per frame
     const gravity = 0.0002;
     const timeDiff = Date.now() - time;
     time = Date.now();
-    if (cats.length === 0) {
+    const frames = Math.min(timeDiff / 16, 2);
+    if (cats.length === 0 || frames < 1) {
         return;
     }
-    const frames = Math.min(timeDiff / 16, 2);
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
     // handle collisions
@@ -114,6 +201,56 @@ const updateCats = (cats: Cat[]) => {
     const catMagnitude = new Array(cats.length).fill(0);
     // handle Collisions with other cats
     const collidedCats = new Set();
+    aliveArig.forEach((arig) => {
+        if (arig.keyFrames != null) {
+            let keyFrame = arig.keyFrames.shift();
+            if (!keyFrame) {
+                arig.keyFrames = undefined;
+                arig.node.src = grab0;
+                const cat = arig.holdingCat;
+                if (cat) {
+                    cat.x = arig.x;
+                    cat.y = arig.y;
+                    cat.xSpeed = arig.xSpeed + Math.random() * 0.02;
+                    cat.ySpeed = arig.ySpeed + Math.random() * 0.02 + 0.01;
+                    cat.node.style.transform = `translate(${cat.x * viewportWidth}px, ${cat.y * viewportHeight}px)`;
+                    aliveCats.add(cat);
+                    cat.node.style.opacity = '';
+                    arig.holdingCat = null;
+                    arig.heldCats.set(cat, Date.now());
+                    arig.dontNeedCat = Date.now() + Math.random() * 3000 + 1000;
+                }
+                return;
+            }
+            keyFrame.time -= timeDiff;
+            if (keyFrame.time <= 0) {
+                keyFrame = arig.keyFrames.shift();
+                if (keyFrame) {
+                    keyFrame.func?.();
+                    arig.node.src = keyFrame.src;
+                }
+            }
+            keyFrame && arig.keyFrames.unshift(keyFrame);
+        } else {
+            const nearestCat = [...aliveCats].filter(cat => {
+                return wantsCat(arig, cat)
+            }).sort((a, b) => {
+                const distA = Math.sqrt((a.x - arig.x) ** 2 + (a.y - arig.y) ** 2);
+                const distB = Math.sqrt((b.x - arig.x) ** 2 + (b.y - arig.y) ** 2);
+                return distA - distB;
+            })[0];
+            if (nearestCat) {
+                // accelerate arig towards nearest cat
+                const dx = nearestCat.x - arig.x;
+                const dy = nearestCat.y - arig.y;
+                const dist = Math.sqrt(dx ** 2 + dy ** 2);
+                const xAccel = dx / dist;
+                const yAccel = dy / dist;
+                arig.xSpeed += xAccel * 0.0001 * frames;
+                arig.ySpeed += yAccel * 0.0001 * frames;
+            }
+        }
+    })
     for (let i = 0; i < cats.length; i++) {
         for (let j = i + 1; j < cats.length; j++) {
             const cat1 = cats[i];
@@ -122,15 +259,54 @@ const updateCats = (cats: Cat[]) => {
             if (!cat1 || !cat2 || cat1 === cat2 || collidedCats.has(hash)) {
                 continue;
             }
+            const padd = 0.70;
             const distance = Math.sqrt((cat1.x - cat2.x) ** 2 + (cat1.y - cat2.y) ** 2);
             const nextDistance = Math.sqrt(((cat1.x + cat1.xSpeed) - (cat2.x + cat2.xSpeed)) ** 2 + ((cat1.y + cat1.ySpeed) - (cat2.y + cat2.ySpeed)) ** 2);
-            if (cat1.x < cat2.x + cat2.width &&
-                cat1.x + cat1.width > cat2.x &&
-                cat1.y < cat2.y + cat2.height &&
-                cat1.y + cat1.height > cat2.y && nextDistance < distance) {
+
+            if (cat1.x < cat2.x + (cat2.width * padd) &&
+                cat1.x + (cat1.width * padd) > cat2.x &&
+                cat1.y < cat2.y + (cat2.height * padd) &&
+                cat1.y + (cat1.height * padd) > cat2.y && nextDistance < distance) {
                 collidedCats.add(hash);
                 // collision detected!
-                
+                if (cat1.type === "arig" && cat1.holdingCat == null && cat2.type === "cat") {
+                    const giveMeThatCat = wantsCat(cat1, cat2);
+                    if (giveMeThatCat) {
+                        cat1.holdingCat = cat2;
+                        aliveCats.delete(cat2);
+                        cat2.node.style.opacity = '0';
+                        if (cat2.text?.node) {
+                            cat2.text.node.style.opacity = '0';
+                        }
+                        cat1.keyFrames = [
+                            {
+                                src: grab0,
+                                time: 0,
+                            },
+                            {
+                                src: grab2,
+                                time: 2000,
+                                func: () => {
+                                    playSound(purr, 'Purrrr', cat1);
+                                }
+                            },
+                            {
+                                src: grab2,
+                                time: 2000,
+                                func: () => {
+                                    playSound(purr, 'Purrrr', cat1);
+                                }
+                            },
+                            {
+                                src: grab3,
+                                time: 2000,
+                                func: () => {
+                                    playSound(kiss, 'kissy!', cat1);
+                                }
+                            }
+                        ]
+                    }
+                }
                 const tempXSpeed = cat1.xSpeed;
                 const tempYSpeed = cat1.ySpeed;
                 cat1.xSpeed = cat2.xSpeed;
@@ -169,13 +345,14 @@ const updateCats = (cats: Cat[]) => {
         cat.xSpeed += (gravity * frames * gravityCoefficient * xAccelCoeff);
         cat.ySpeed += (gravity * frames * gravityCoefficient * yAccelCoeff);
         cat.node.style.transform = `translate(${cat.x * viewportWidth}px, ${cat.y * viewportHeight}px)`;
-
-        if ((cat.meowChance * frames)> Math.random()) {
-            const indx = Math.floor(Math.random() * 4)
-            playSound(meowSrcs[indx], catSounds[indx], cat, (Math.random() * 0.5) + 0.5);
-            cat.meowChance = 0;
-        } else {
-            cat.meowChance += 0.000025 * frames;
+        if (cat.type === "cat") {
+            if ((cat.meowChance * frames)> Math.random()) {
+                const indx = Math.floor(Math.random() * meowSrcs.length)
+                playSound(meowSrcs[indx], catSounds[indx], cat, (Math.random() * 0.5) + 0.5);
+                cat.meowChance = 0;
+            } else {
+                cat.meowChance += 0.000025 * frames;
+            }
         }
     });
     cats.forEach((cat) => {
@@ -211,7 +388,8 @@ const updateCats = (cats: Cat[]) => {
     })
 
 }
-const aliveCats: Cat[] = [];
+const aliveCats: Set<Cat> = new Set;
+const aliveArig: Arig[] = [];
 const preloadedCats = [];
 const preloadedAudio = [];
 export const CATS = () => {
@@ -225,18 +403,17 @@ export const CATS = () => {
         img.src = src;
         preloadedCats.push(img);
     });
-    [...meowSrcs, yippee, bonk].forEach((src) => {
-        const audio = new Audio();
-        audio.src = src;
-        preloadedAudio.push(audio);
-        audio.load();
+    [...meowSrcs, yippee, bonk, kiss, purr].forEach((src) => {
+        audioCache.set(src, [
+            new Audio(src),
+        ]);
     });
 
   }
   let stop = false;
 
   const renderFrame = () => {
-    updateCats(aliveCats);
+    updateEntities(parent, [...aliveArig, ...aliveCats]);
     if (!stop) {
         requestAnimationFrame(renderFrame);
     }
@@ -246,9 +423,16 @@ export const CATS = () => {
     const y = event.clientY;
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
+    if (aliveCats.size === 5 && aliveArig.length === 0) {
+        createArig(x/viewportWidth, y/viewportHeight).then((cat) => {
+            parent.appendChild(cat.node);
+            aliveArig.push(cat);
+        });
+        return;
+    }
     createCat(x/viewportWidth, y/viewportHeight).then((cat) => {
         parent.appendChild(cat.node);
-        aliveCats.push(cat);
+        aliveCats.add(cat);
     });
   }
   window.addEventListener("pointerup", handleTouchStart);
